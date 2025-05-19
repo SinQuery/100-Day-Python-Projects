d387c3162b464f03c872503dff5e287862c022a9
SinQuery <sinquanotsinkwa@gmail.com>
2025-05-19 22:24:13 +0200

    First commit

---
+++ Day_3/Project/Treasure_Island.py
@@ -0,0 +1,5 @@
+print("Welcome to Treaure Island.\nYour mission is to find the treasure.")
+direction = input("Which direction would you like to head, left or right?\n")
+direction.lower()
+if direction != "left":
+    print("Not left")

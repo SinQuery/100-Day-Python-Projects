77fbd1c9ed9c5fac7a6d2f60108e2a61749f54fe
SinQuery <sinquanotsinkwa@gmail.com>
2025-05-22 10:15:08 +0200

    Final submission. Functional.
    Had to revert from using if-not conditonal, as it lead to a process flow problem, whereby every question/decision had to be syntatically followed by a newly defined if-statement, therefore continuing the loop even if the previous outcome was GAME OVER. Also used color syntax in my print statement so just be aware of that. Otherwise all possible outcomes are covered as specified in the diagram.

--- Day_3/Project/Treasure_Island.py
+++ Day_3/Project/Treasure_Island.py
@@ -1,5 +1,19 @@
 print("Welcome to Treaure Island.\nYour mission is to find the treasure.")
 direction = input("Which direction would you like to head, left or right?\n")
-direction.lower()
-if direction != "left":
-    print("Not left")
+direction = direction.lower()
+if direction.lower() == "left":
+    act = input("Would you like to swim to the next point, or wait?\n")
+    if act.lower() == "wait":
+        door = input("Which door would you like to use, Red, Yellow or Blue\n")
+        if door.lower() == "yellow":
+            print("\033[92mYou Win!\033[0m")
+        elif door.lower() == "red":
+            print("You have been burned by fire.\n\033[91mGAME OVER\033[0m")
+        elif door.lower() == "blue":
+            print("You have been eaten by beasts.\n\033[91mGAME OVER\033[0m")
+        else: 
+            print("\033[91mGAME OVER")
+    else: 
+        print("You've been attacked by trout.\n\033[91mGAME OVER!\033[0m")
+else: 
+    print("You have fallen into a hole.\n\033[91mGAME OVER!\033[0m")
